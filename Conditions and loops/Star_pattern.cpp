0c62311fc249997f210994ed104e9b10338caffd
Vaishnavi Mokadam <89184872+Vaishnavi2701-mk@users.noreply.github.com>
2022-03-05 18:02:20 +0530

    Create Star_pattern.cpp

---
+++ Conditions and loops/Star_pattern.cpp
@@ -0,0 +1,56 @@
+/*Start Pattern
+
+Print the following pattern
+Pattern for N = 4
+
+
+The dots represent spaces.
+
+
+Input Format :
+N (Total no. of rows)
+Output Format :
+Pattern in N lines
+Constraints :
+0 <= N <= 50
+Sample Input 1 :
+3
+Sample Output 1 :
+   *
+  *** 
+ *****
+Sample Input 2 :
+4
+Sample Output 2 :
+    *
+   *** 
+  *****
+ *******
+ */
+ 
+ #include<iostream>
+using namespace std;
+
+
+int main(){
+int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int k=1;
+        while(k<=n-i){
+            cout<<" ";
+            k=k+1;
+        }
+        int j=1;
+        while(j<=2*i-1){
+            cout<<"*";
+            j=j+1;
+            
+        }
+        cout<<endl;
+        i=i+1;
+      
+  
+}
+}
