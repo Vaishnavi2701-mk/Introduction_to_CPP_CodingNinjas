0d258a50d1ba23f00f016112eaf8eff6772f477e
Vaishnavi Mokadam <89184872+Vaishnavi2701-mk@users.noreply.github.com>
2022-03-05 20:58:40 +0530

    Create Reverse-of-Numbers.cpp

---
+++ Operators-and-for-loops/Reverse-of-Numbers.cpp
@@ -0,0 +1,38 @@
+/* Reverse of Numbers
+Write a program to generate the reverse of a given number N. Print the corresponding reverse number.
+Note : If a number has trailing zeros, then its reverse will not include them. For e.g., reverse of 10400 will be 401 instead of 00401.
+
+Input format :
+Integer N
+Output format :
+Corresponding reverse number
+Constraints:
+0 <= N < 10^8
+Sample Input 1 :
+1234
+Sample Output 1 :
+4321
+Sample Input 2 :
+1980
+Sample Output 2 :
+891
+
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+int n;
+cin>>n;
+int r=0;
+while(n>0){
+    int u=n%10;
+    r=r*10+u;
+    n=n/10;
+}
+cout<<r<<endl;
+	
+}
+
