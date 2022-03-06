35a63d7307d84380dfc527f2d6b8fd931fbd9745
Vaishnavi Mokadam <89184872+Vaishnavi2701-mk@users.noreply.github.com>
2022-03-06 18:25:23 +0530

    Create Second_Largest.cpp

---
+++ Test 1/Second_Largest.cpp
@@ -0,0 +1,47 @@
+/* second largest
+
+Take input a stream of n integer elements, find the second largest element present.
+The given elements can contain duplicate elements as well. If only 0 or 1 element is given, the second largest should be INT_MIN ( - 2^31 ).
+Input format :
+Line 1 : Total number of elements (n)
+Line 2 : N elements (separated by space)
+Sample Input 1:
+ 4
+ 3 9 0 9
+Sample Output 1:
+ 3
+Sample Input 2 :
+ 2
+ 4 4
+Sample Output 2:
+ -2147483648
+Sample Output Explanation:
+Since both the elements are equal here, hence second largest element is INT_MIN i.e. ( -2^31 )*/
+
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+int n,i;
+cin>>n;
+int arr[n];
+for(i=0;i<n;i++){
+    cin>>arr[i];
+}
+int max=INT_MIN;
+int second_max=INT_MIN;
+for(i=0;i<n;i++){
+    if(arr[i]>max){
+        second_max=max;
+        max=arr[i];
+
+    }
+    if(arr[i]<max&&arr[i]>second_max){
+        second_max=arr[i];
+
+    }
+
+}
+cout<<second_max;
+return 0;
+}
