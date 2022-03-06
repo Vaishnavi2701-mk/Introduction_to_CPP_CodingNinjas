282dd7a2df3f313bb9816a6463894bcbc65debd0
Vaishnavi Mokadam <89184872+Vaishnavi2701-mk@users.noreply.github.com>
2022-03-06 18:24:17 +0530

    Create Number_star_pattern.cpp

---
+++ Test 1/Number_star_pattern.cpp
@@ -0,0 +1,43 @@
+/* Number star pattern
+
+Print the following pattern for given number of rows.
+Input format :
+Line 1 : N (Total number of rows)
+Sample Input :
+   5
+Sample Output :
+1234554321
+1234**4321
+123****321
+12******21
+1********1
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+int i=1;
+while(i<=n){
+    int j=1;
+    while(j<=n-i+1){
+        cout<<j;
+        j++;
+    }
+    j=0;
+    while(j<i*2-2){
+        cout<<"*";
+        j++;
+    }
+    j=n-i+1;
+    while(j>=1){
+        cout<<j;
+        j--;
+    }
+   cout<<endl;
+   i++;
+}
+}
+
+
